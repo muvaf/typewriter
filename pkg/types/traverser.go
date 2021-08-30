781d614a5ba42ec280528dc1b390382632d5637a
Muvaffak Onus <me@muvaf.com>
2021-08-30 20:29:35 +0300

    types.traverser: add more cases to the traversal tree
    Signed-off-by: Muvaffak Onus <me@muvaf.com>

--- pkg/types/traverser.go
+++ pkg/types/traverser.go
@@ -22,29 +22,47 @@ import (
 	"github.com/muvaf/typewriter/pkg/packages"
 )
 
-func NewTraverser(cache *packages.Cache, typeProcessors TypeProcessorChain, fieldProcessors FieldProcessorChain) *Traverser {
-	return &Traverser{
-		cache:           cache,
-		commentCache:    packages.NewCommentCache(cache),
-		TypeProcessors:  typeProcessors,
-		FieldProcessors: fieldProcessors,
+func WithFieldProcessor(fp FieldProcessor) TraverserOption {
+	return func(t *Traverser) {
+		t.FieldProcessors = fp
 	}
 }
 
+func WithTypeProcessor(tp TypeProcessor) TraverserOption {
+	return func(t *Traverser) {
+		t.TypeProcessors = tp
+	}
+}
+
+type TraverserOption func(*Traverser)
+
+func NewTraverser(cache *packages.Cache, opts ...TraverserOption) *Traverser {
+	t := &Traverser{
+		cache:           cache,
+		commentCache:    packages.NewCommentCache(cache),
+		TypeProcessors:  TypeProcessorChain{},
+		FieldProcessors: FieldProcessorChain{},
+	}
+	for _, f := range opts {
+		f(t)
+	}
+	return t
+}
+
 type Traverser struct {
-	TypeProcessors  TypeProcessorChain
-	FieldProcessors FieldProcessorChain
+	TypeProcessors  TypeProcessor
+	FieldProcessors FieldProcessor
 
 	cache        *packages.Cache
 	commentCache *packages.CommentCache
 }
 
 func (t *Traverser) Traverse(n *types.Named, formerFields ...string) error {
-	pComments, err := t.commentCache.GetComments(n.Obj().Pkg().Path())
+	pComments, err := t.commentCache.GetPackageComments(n.Obj().Pkg().Path())
 	if err != nil {
 		return errors.Wrapf(err, "cannot get comments for package %s", n.Obj().Pkg().Path())
 	}
-	if err := t.TypeProcessors.Process(n, pComments[n.Obj()]); err != nil {
+	if err := t.TypeProcessors.Process(n, pComments.CommentOf(n.Obj())); err != nil {
 		return errors.Wrapf(err, "type processors failed to run for type %s", n.Obj().Name())
 	}
 	st, ok := n.Underlying().(*types.Struct)
@@ -54,15 +72,62 @@ func (t *Traverser) Traverse(n *types.Named, formerFields ...string) error {
 	for i := 0; i < st.NumFields(); i++ {
 		field := st.Field(i)
 		tag := st.Tag(i)
-		if err := t.FieldProcessors.Process(n, field, tag, pComments[field], formerFields); err != nil {
+		if err := t.FieldProcessors.Process(n, field, tag, pComments.CommentOf(field), formerFields); err != nil {
 			return errors.Wrapf(err, "field processors failed to run for field %s of type %s", field.Name(), n.Obj().Name())
 		}
-		ft, ok := field.Type().(*types.Named)
-		if !ok {
-			continue
-		}
-		if err := t.Traverse(ft, append(formerFields, field.Name())...); err != nil {
-			return errors.Wrapf(err, "failed to traverse type of field %s", field.Name())
+		switch ft := field.Type().(type) {
+		case *types.Named:
+			if err := t.Traverse(ft, append(formerFields, field.Name())...); err != nil {
+				return errors.Wrapf(err, "failed to traverse type of field %s", field.Name())
+			}
+		case *types.Pointer:
+			switch elemType := ft.Elem().(type) {
+			case *types.Named:
+				if err := t.Traverse(elemType, append(formerFields, "*", field.Name())...); err != nil {
+					return errors.Wrapf(err, "failed to traverse type of field %s", field.Name())
+				}
+			}
+		case *types.Slice:
+			switch elemType := ft.Elem().(type) {
+			case *types.Named:
+				if err := t.Traverse(elemType, append(formerFields, "[]", field.Name())...); err != nil {
+					return errors.Wrapf(err, "failed to traverse type of field %s", field.Name())
+				}
+			case *types.Pointer:
+				switch elemElemType := elemType.Elem().(type) {
+				case *types.Named:
+					if err := t.Traverse(elemElemType, append(formerFields, "[]", "*", field.Name())...); err != nil {
+						return errors.Wrapf(err, "failed to traverse type of field %s", field.Name())
+					}
+				}
+			}
+		case *types.Map:
+			switch elemType := ft.Elem().(type) {
+			case *types.Named:
+				if err := t.Traverse(elemType, append(formerFields, "[mapvalue]", field.Name())...); err != nil {
+					return errors.Wrapf(err, "failed to traverse type of field %s", field.Name())
+				}
+			case *types.Pointer:
+				switch elemElemType := elemType.Elem().(type) {
+				case *types.Named:
+					if err := t.Traverse(elemElemType, append(formerFields, "[mapvalue]", "*", field.Name())...); err != nil {
+						return errors.Wrapf(err, "failed to traverse type of field %s", field.Name())
+					}
+				}
+			}
+			switch keyType := ft.Key().(type) {
+			case *types.Named:
+				if err := t.Traverse(keyType, append(formerFields, "[mapkey]", field.Name())...); err != nil {
+					return errors.Wrapf(err, "failed to traverse type of field %s", field.Name())
+				}
+			case *types.Pointer:
+				switch elemKeyType := keyType.Elem().(type) {
+				case *types.Named:
+					if err := t.Traverse(elemKeyType, append(formerFields, "[mapkey]", "*", field.Name())...); err != nil {
+						return errors.Wrapf(err, "failed to traverse type of field %s", field.Name())
+					}
+				}
+			}
 		}
 	}
 	return nil
